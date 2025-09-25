ed047239f62a9a1311c471e7efdaa0b482570464
Lavanya <dhanulavy811@gmail.com>
2025-09-25 19:03:55 +0530

    second commit

--- main.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const btn = document.getElementById("clickMe");
-  btn.addEventListener("click", function() {
-    alert("Hello! You clicked the button 🚀");
-  });
-});
